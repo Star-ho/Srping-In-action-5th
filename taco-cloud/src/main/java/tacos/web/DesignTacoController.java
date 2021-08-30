e2239df2dedd70a6686ffa63dfce07d868268a64
Star-ho <58648988+Star-ho@users.noreply.github.com>
2021-08-30 23:53:04 +0900

    taco JPA

--- taco-cloud/src/main/java/tacos/web/DesignTacoController.java
+++ taco-cloud/src/main/java/tacos/web/DesignTacoController.java
@@ -73,6 +73,7 @@ public class DesignTacoController {
     public String processDesign(@Valid Taco design,Errors errors,@ModelAttribute Order order){
         if(errors.hasErrors()){
             return "design";
+
         }
         Taco saved=tacoRepo.save(design);
         order.addDesign(saved);
